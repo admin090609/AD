a998f28e692777238c988a2c67fe9fabf27f2db9
Loghin Dan <loghindan494@gmail.com>
2023-09-08 16:46:08 +0300

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ import Footer from "./component/Footer";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="icon" href="C:\Users\User\Desktop\AD\public\favicon.ico" type="image/x-icon" />
+
+        </Head>
       <body>
         <Main />
         <Footer/>
