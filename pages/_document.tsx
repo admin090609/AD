8a073b74065e054cf06fb00e05ab71db4fd7784a
Loghin Dan <loghindan494@gmail.com>
2023-09-08 18:50:24 +0300

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,7 @@ import Footer from "./component/Footer";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <link rel="icon" href="C:\Users\User\Desktop\AD\public\favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="mask-icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" href="favicon.ico" type="image/x-icon" />
-
-        </Head>
+      <Head />
       <body>
         <Main />
         <Footer/>
