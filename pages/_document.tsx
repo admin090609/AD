09ade7b6e6d006450edc90bfb86992c8c9ebca63
Loghin Dan <loghindan494@gmail.com>
2023-09-08 16:50:56 +0300

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link rel="icon" href="C:\Users\User\Desktop\AD\public\favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="mask-icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="apple-touch-icon" href="favicon.ico" type="image/x-icon" />
 
         </Head>
       <body>
