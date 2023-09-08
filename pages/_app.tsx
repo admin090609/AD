819007ec8313905466d91c672fa1c6d72d71ac3a
Loghin Dan <loghindan494@gmail.com>
2023-09-08 12:27:30 +0300

    u[date

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-      <link rel="icon" href="logo.png"/>
         <link rel="icon" href="favicon.ico" type="image/x-icon"/>
         <meta
           name="description"
